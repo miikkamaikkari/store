16ab84253758e74b1cb3ea08314fc64af5126253
miikkamaikkari <147990144+miikkamaikkari@users.noreply.github.com>
2023-11-09 10:32:59 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+
+    sections.forEach(section => {
+        section.addEventListener('mouseenter', function() {
+            this.style.transform = 'scale(1.1)';
+            this.style.transition = 'transform 0.5s';
+        });
+
+        section.addEventListener('mouseleave', function() {
+            this.style.transform = 'scale(1)';
+        });
+    });
+
+    const carousel = document.querySelector('.carousel');
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
+
+    let currentIndex = 0;
+
+    nextBtn.addEventListener('click', () => {
+        if (currentIndex < 2) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+        updateCarousel();
+    });
+
+    prevBtn.addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = 2;
+        }
+        updateCarousel();
+    });
+
+    function updateCarousel() {
+        const newPosition = -currentIndex * 320;
+        carousel.style.transform = `translateX(${newPosition}px)`;
+    }
+
+    const productCards = document.querySelectorAll('.product-card');
+
+    productCards.forEach(card => {
+        card.addEventListener('click', function() {
+            alert('Product added to cart!');
+        });
+    });
+});
